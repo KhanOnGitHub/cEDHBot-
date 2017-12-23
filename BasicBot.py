95828ba32e0b84617f4d9b5cb4de71e410ea81f4
Momo <snillmomo@gmail.com>
2017-12-23 14:06:01 -0500

    Update readme. Create statements. ERD

---
+++ BasicBot.py
@@ -0,0 +1,40 @@
+# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+client = Bot(description="Basic Bot by Habchy#1665 followed by Snilly", command_prefix="-", pm_help = True)
+
+# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
+# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+	print('--------')
+	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+	print('--------')
+	print('Use this link to invite {}:'.format(client.user.name))
+	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+	print('--------')
+	print('Support Discord Server: https://discord.gg/FNNNgqb')
+	print('Github Link: https://github.com/Habchy/BasicBot')
+	print('--------')
+	print('Created by Habchy#1665')
+
+# This is a basic example of a call and response command. You tell it do "this" and it does it.
+@client.command()
+async def ping(*args):
+	await client.say(":ping_pong: Pong!")
+
+@client.command()
+async def hello(*args):
+	await client.say("Hello World!")
+	
+	
+client.run('MzkzNTE3MDMxOTc2MDc1Mjg1.DR27BA.79I46Qy9U9eRKyETkIQ2aLgB-Vw')
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cEDHBot-
+# cEDHBot- Bot to keep track of cEDH games WIP.
+
+1. ERD: Done
+2. Relational database creation: DONE
+3. Prepare demo data:
+4. Insert demo data:
+5. Create prepared statements:
+6. Debug statements/db: 
+7. Make discord bot:
